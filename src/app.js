801ec8e29418ccf4fc613330d1a3f66f3a1c8e8b
Ronalson Filho <contact@ronalson.com.br>
2017-11-07 23:06:45 -0200

    Implement interview object and adds it to listing.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import ko from './vendor/knockout-3.4.2.js';
+import interviewObject from './js/interview-object.js';
 
-const AppViewModel = function(interviews, map) {
-  this.interviews = ko.observableArray(interviews);
-  this.selectInterview = function(interview) {
-    alert(interview.url);
-  }
+const AppViewModel = function (interviews, map) {
+
+  var self = this;
+
+  this.markersArray = ko.observableArray();
+  this.visibleMarkersArray = ko.observableArray();
+  // Fill markersArray with data from JSON
+  interviews.forEach(function (data) {
+    self.markersArray.push(new interviewObject(data, map));
+  });
+
+  // Set initial list from array
+  this.markersArray().forEach(function (data) {
+    self.visibleMarkersArray.push(data);
+  });
 }
+  
+};
 
 export default AppViewModel;
